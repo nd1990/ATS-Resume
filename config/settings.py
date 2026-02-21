ae3f7f07286a39cb19a05d2d39a66c67e46645bb
Nilu1990 <nilesh.d@masterlysolutions.com>
2026-02-21 17:27:35 +0530

    Refactor database settings to use absolute path for SQLite and add timeout option

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# Absolute path so DB connects no matter where you run the server from
+import os
+_db_path = os.path.join(str(BASE_DIR), 'db.sqlite3')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': _db_path,
+        'OPTIONS': {'timeout': 20},
     }
 }
 
